6a7d6551efa3439214e71f841c2c3e644a7faa04
Ivaylo Ivanov <ivchoyyyy@gmail.com>
2015-12-01 16:49:05 +0200

    removed manager and updated gruntfile

--- apps/server/Gruntfile.js
+++ apps/server/Gruntfile.js
@@ -1,81 +1,52 @@
 module.exports = function (grunt) {
   grunt.initConfig({
+    concurrent: {
+        target: ['node-inspector', 'nodemon'],
+        options: {
+          logConcurrentOutput: true
+        }
+    },
     'node-inspector': {
       dev: {}
     },
     nodemon: {
       all: {
-        script: 'server.js',
+        script: 'app.js',
         options: {
           ignored: ['README.md', 'node_modules/**'],
-          watchedExtensions: ['js', 'md'],
-          watchedFolders: ['test', 'tasks'],
+          watchedExtensions: ['js', 'json'],
+          watchedFolders: ['test', 'app'],
           delayTime: 1,
           nostdin: true,
           exitcrash: true,
           legacyWatch: true,
-          env: {
-            PORT: '8181'
-          },
-          args: ['production'],
           nodeArgs: ['--debug'],
           callback: function(nodemon) {
             nodemon.on('log', function(event) {
               console.log(event.colour);
             });
-            nodemon.on('config:update', function(event) {
-              // console.log('custom logging');
-              // console.log(event);
-            });
           }
         }
       },
-      none: {
-        script: 'test/fixtures/server.js',
-      }
+      none: {}
     },
-    jshint: {
+    eslint: {
       options: {
-        bitwise: true,
-        indent: 20,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        nonew: true,
-        quotmark: 'single',
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        trailing: true,
-        eqnull: true,
-        node: true,
-        expr: true,
-        evil: true,
-        globals: {
-          describe: false,
-          it: false,
-          before: false
-        }
+          configFile: '.eslintrc',
+          rulePaths: ['']
       },
-      files: {
-        src:  ['*.js', 'test/*.js', 'tasks/*.js']
+      all: {
+        src: ['app/']
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-nodemon');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-force-task');
   grunt.loadNpmTasks('grunt-node-inspector');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-force-task');
+  grunt.loadNpmTasks('grunt-concurrent');
 
-  // grunt.loadNpmTasks('grunt-node-inspector');
-
-  // grunt.registerTask('default', ['node-inspector']);
-  grunt.registerTask('default', ['node-inspector', 'force:jshint', 'nodemon']);
-  // grunt.registerTask('test', ['jshint']);
-
+  grunt.registerTask('default', ['force:eslint', 'concurrent:target']);
 
 };
