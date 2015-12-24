1d7907757b9248a37c6e75d11998fa8aeea1975c
Ivaylo Ivanov <ivchoyyyy@gmail.com>
2015-12-24 21:27:29 +0200

    added comments

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 let Esrol = require('./lib/esrol');
 let path = require('path');
-let tests = process.argv[2] === '--tests'
-let absPath = process.argv[1];
+let tests = process.argv[2] === '--tests';
 let modulePath = tests ? path.join('tests', 'out') : null;
-new Esrol(absPath, modulePath);
+new Esrol(__dirname, modulePath);
